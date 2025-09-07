672b6c44d0cd170b5ba570feadae2b1cc204241a
rakun <rahulkunte7@gmail.com>
2025-09-07 20:06:32 +0200

    added config file

---
+++ config.py
@@ -0,0 +1,31 @@
+headers = {
+
+
+}
+
+
+cookies = {
+
+
+
+}
+
+
+
+prop_config = {
+    "sale": {
+        "url": "https://www.immoweb.be/en/search-results/{}/for-sale/{}/{}?page={}",
+        # "prop_types": ['house-and-apartment', 'new-real-estate-projects', 'garage',
+        #                'office', 'business', 'industry', 'land', 'tenement', 'other']
+        "prop_types": ['land']
+    },
+
+    "rent": {
+        "url": "https://www.immoweb.be/en/search-results/{}/for-rent/{}/{}?page={}",
+        # "prop_types": ['house-and-apartment', 'garage',
+        #                'office', 'business', 'industry', 
+        #                'land', 'other']     
+        "prop_types": ["land"]
+    }
+
+}
